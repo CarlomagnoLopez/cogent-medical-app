b1929c55183c9786e7b64a11fa1da1551099ffea
Mahesh Giri <maheshgiri@users.noreply.github.com>
2020-06-03 02:32:07 +0530

    key names changed

--- src/pages/siteorg/orgs/index.js
+++ src/pages/siteorg/orgs/index.js
@@ -58,8 +58,8 @@ class Org extends Component {
       }),
     });*/
     AWS.config.update({
-      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+      accessKeyId: process.env.ACCESS_KEY_ID,
+      secretAccessKey: process.env.SECRET_ACCESS_KEY,
     });
     s3 = new AWS.S3({
       apiVersion: '2006-03-01',
